6bb53c52484f8b02de53591316973f7879f71f76
Ju Liu <ju.liu@welaika.com>
2014-03-30 12:13:11 +0200

    Change default decorator

--- lib/priscilla.rb
+++ lib/priscilla.rb
@@ -1,13 +1,23 @@
 require 'colorize'
 
-require "priscilla/version"
-require "priscilla/simple_emoji"
-require "priscilla/configuration"
-require "priscilla/makeup"
+require 'priscilla/version'
+require 'priscilla/simple_emoji'
+require 'priscilla/configuration'
+require 'priscilla/makeup'
+
+module Priscilla
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+end
 
 module Kernel
   def pr(message, **options)
-    puts Priscilla::Makeup.new(Priscilla.configuration).decorate(message, options)
-    puts
+    puts Priscilla::Makeup.new(Priscilla.configuration)
+      .decorate(message, options) + "\n"
   end
 end
--- lib/priscilla/configuration.rb
+++ lib/priscilla/configuration.rb
@@ -1,21 +1,13 @@
 module Priscilla
-  def self.configuration
-    @configuration ||= Configuration.new
-  end
-
-  def self.configure
-    yield(configuration)
-  end
-
   class Configuration
     DEFAULT_WIDTH = 80
-    DEFAULT_DECORATOR = "⚡ "
+    DEFAULT_DECORATOR = ':dancer: '
 
     attr_accessor :width, :decorator
 
     def initialize
-      @width = DEFAULT_WIDTH
-      @decorator = DEFAULT_DECORATOR
+      self.width = DEFAULT_WIDTH
+      self.decorator = DEFAULT_DECORATOR
     end
 
     def decorator=(value)
